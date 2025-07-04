964477a14ef9575e5b901bc33df0aa2a22889cf0
techbase2brand <techbase2brand@gmail.com>
2025-07-04 17:27:03 +0530

    test

--- src/app/brands/[...slug]/BookSlot.tsx
+++ src/app/brands/[...slug]/BookSlot.tsx
@@ -128,12 +128,12 @@ const BookSlot: React.FC<BookSlotProps> = ({
         comment: formData.comment,
         address_no:
           formData.storeLocation ===
-          "122 Queen St, St Marys NSW 2760, Australia"
+            "122 Queen St, St Marys NSW 2760, Australia"
             ? `(02) 8678 3298`
             : `(02) 7252 7141`,
         direction_url:
           formData.storeLocation ===
-          "122 Queen St, St Marys NSW 2760, Australia"
+            "122 Queen St, St Marys NSW 2760, Australia"
             ? `https://www.google.com/maps/place/122+Queen+St,+St+Marys+NSW+2760,+Australia/@-33.7586704,150.756797,5340m/data=!3m1!1e3!4m6!3m5!1s0x6b129ab1929d4f97:0x103feb9784d7d023!8m2!3d-33.766127!4d150.7743917!16s%2Fg%2F11b8y9vjx6?entry=ttu&g_ep=EgoyMDI1MDYwMi4wIKXMDSoASAFQAw%3D%3D`
             : `https://www.google.com/maps/place/TechCity+Schofields+Phone+Repair/@-33.7057889,150.8759664,859m/data=!3m2!1e3!4b1!4m6!3m5!1s0x6b129f1f16bc4395:0xf55b68eeebf4e2e2!8m2!3d-33.7057889!4d150.8759664!16s%2Fg%2F11smlzr8dn?entry=ttu&g_ep=EgoyMDI1MDYzMC4wIKXMDSoASAFQAw%3D%3D`,
       };
@@ -212,18 +212,16 @@ const BookSlot: React.FC<BookSlotProps> = ({
                     name="name"
                     value={formData.name}
                     onChange={handleInputChange}
-                    className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${
-                      errors.name ? "border-red-500" : "border-prc "
-                    } focus:ring-prc`}
+                    className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${errors.name ? "border-red-500" : "border-prc "
+                      } focus:ring-prc`}
                     placeholder="John Doe"
                   />
                   <label
                     htmlFor="name"
-                    className={` leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${
-                      formData.name || errors.name
+                    className={` leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${formData.name || errors.name
                         ? "scale-100 top-0 text-prc"
                         : "scale-100 text-prc "
-                    }`}
+                      }`}
                   >
                     Name <span className="text-red-500">*</span>
                   </label>
@@ -239,18 +237,16 @@ const BookSlot: React.FC<BookSlotProps> = ({
                       name="mobile"
                       value={formData.mobile}
                       onChange={handleInputChange}
-                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus  ${
-                        errors.mobile ? "border-red-500" : " border-prc "
-                      } focus:ring-prc`}
+                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus  ${errors.mobile ? "border-red-500" : " border-prc "
+                        } focus:ring-prc`}
                       placeholder="Enter Your Mobile Number"
                     />
                     <label
                       htmlFor="mobile"
-                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${
-                        formData.mobile || errors.mobile
+                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${formData.mobile || errors.mobile
                           ? "scale-100 top-0 text-prc"
                           : "scale-100 text-prc "
-                      }`}
+                        }`}
                     >
                       Mobile <span className="text-red-500">*</span>
                     </label>
@@ -268,18 +264,16 @@ const BookSlot: React.FC<BookSlotProps> = ({
                       name="email"
                       value={formData.email}
                       onChange={handleInputChange}
-                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${
-                        errors.email ? "border-red-500" : "border-prc "
-                      }  focus:ring-prc`}
+                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${errors.email ? "border-red-500" : "border-prc "
+                        }  focus:ring-prc`}
                       placeholder="abc@mail.com"
                     />
                     <label
                       htmlFor="email"
-                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200  md:text-[16px]  text-[13px] ${
-                        formData.email || errors.email
+                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200  md:text-[16px]  text-[13px] ${formData.email || errors.email
                           ? "scale-100 top-0 text-prc"
                           : "scale-100 text-prc "
-                      }`}
+                        }`}
                     >
                       Email <span className="text-red-500">*</span>
                     </label>
@@ -289,37 +283,29 @@ const BookSlot: React.FC<BookSlotProps> = ({
                   </div>
 
                   {/* Date Field */}
-                  <div className="relative mb-2">
-                    <input
-                      type="date"
-                      name="date"
-                      value={formData.date}
-                      onChange={handleInputChange}
-                      min={today}
-                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus  overflow-hidden box-border profile-label-rs ${
-                        errors.date ? "border-red-500" : "border-prc "
-                      }  focus:ring-prc`}
-                     style={{
-                            width: "100%",
-                            maxWidth: "100%",
-                         
-                          }}
- 
-                    />
-                    <label
-                      htmlFor="date"
-                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200  md:text-[16px]  text-[13px] ${
-                        formData.date || errors.date
-                          ? "scale-100 top-0 text-prc"
-                          : "scale-100 text-prc "
-                      }`}
-                    >
-                      Date <span className="text-red-500">*</span>
-                    </label>
-                    {errors.date && (
-                      <p className="text-red-500 text-sm m-0">{errors.date}</p>
-                    )}
+                  <div className="mb-2 ">
+                    <div className="relative w-full max-w-full m-auto">
+                      <input
+                        type="date"
+                        name="date"
+                        value={formData.date}
+                        onChange={handleInputChange}
+                        min={today}
+                        className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc focus:border-prc custom-focus overflow-hidden box-border ${errors.date ? "border-red-500" : "border-prc"} focus:ring-prc`}
+                        style={{ appearance: "none" }}
+                      />
+                      <label
+                        htmlFor="date"
+                        className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px] text-[13px] ${formData.date || errors.date ? "scale-100 top-0 text-prc" : "scale-100 text-prc"}`}
+                      >
+                        Date <span className="text-red-500">*</span>
+                      </label>
+                      {errors.date && (
+                        <p className="text-red-500 text-sm m-0">{errors.date}</p>
+                      )}
+                    </div>
                   </div>
+
 
                   {/* Time Slot Dropdown */}
                   <div className="relative mb-2">
@@ -327,9 +313,8 @@ const BookSlot: React.FC<BookSlotProps> = ({
                       name="timeSlot"
                       value={formData.timeSlot}
                       onChange={handleInputChange}
-                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${
-                        errors.timeSlot ? "border-red-500" : "border-prc "
-                      }  focus:ring-prc`}
+                      className={`md:h-12 h-10 text-base peer w-full p-2 border rounded border-prc  focus:border-prc custom-focus ${errors.timeSlot ? "border-red-500" : "border-prc "
+                        }  focus:ring-prc`}
                     >
                       <option value="">Select</option>
                       {formData.date && (
@@ -356,11 +341,10 @@ const BookSlot: React.FC<BookSlotProps> = ({
                     </select>
                     <label
                       htmlFor="timeSlot"
-                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${
-                        formData.timeSlot || errors.timeSlot
+                      className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${formData.timeSlot || errors.timeSlot
                           ? "scale-100 top-0 text-prc"
                           : "scale-100 text-prc "
-                      }`}
+                        }`}
                     >
                       Time Slot <span className="text-red-500">*</span>
                     </label>
@@ -433,11 +417,10 @@ const BookSlot: React.FC<BookSlotProps> = ({
                   />
                   <label
                     htmlFor="phoneColor"
-                    className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 text-prc md:text-[16px]  text-[13px] ${
-                      formData.phoneColor
+                    className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 text-prc md:text-[16px]  text-[13px] ${formData.phoneColor
                         ? "scale-100 top-0 text-prc"
                         : "scale-100 text-prc"
-                    }`}
+                      }`}
                   >
                     Phone Color
                   </label>
@@ -455,11 +438,10 @@ const BookSlot: React.FC<BookSlotProps> = ({
                   />
                   <label
                     htmlFor="comment"
-                    className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${
-                      formData.comment
+                    className={`leading-5 absolute left-2 top-0 bg-white transform -translate-y-1/2 transition-all duration-200 md:text-[16px]  text-[13px] ${formData.comment
                         ? "scale-100 top-0 text-prc"
                         : "scale-100 text-prc "
-                    }`}
+                      }`}
                   >
                     Comment
                   </label>
